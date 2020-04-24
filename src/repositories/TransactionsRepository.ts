ffbe2788ecdcc0443efbe0edf97c83cb3b153059
SirKadogan <thaleskenne@hotmail.com>
2020-04-23 21:31:49 -0300

    Finished challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeArray = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomeArray = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    let income = 0;
+    let outcome = 0;
+
+    incomeArray.forEach(transaction => {
+      income += transaction.value;
+    });
+    outcomeArray.forEach(transaction => {
+      outcome += transaction.value;
+    });
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
